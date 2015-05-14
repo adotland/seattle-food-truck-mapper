eff0c3cf4cd08040a58bdecb6831f3b048c4b146
adotland <a@dotlandconsulting.com>
2015-05-14 15:33:35 -0700

    only clear cache for a day, not a week
    want to remove this once caching issue solved

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
-var c = function() {
-            if (typeof(chrome.browsingData) !== 'undefined') {
-                clearRunning = true;
-                var millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-                var aboutAWeekAgo = (new Date()).getTime() - millisecondsPerWeek;
-                
-                chrome.browsingData.removeCache({
-                      "since": aboutAWeekAgo
-                    }, function() {
-                    clearRunning = false;
-                });
-            } else {
-                alert("Your browser does not support 'chrome.browsingData.removeCache'.");
-            }
-        
-    };
+var clearCache = function () {
+	if (typeof(chrome.browsingData) !== 'undefined') {
+		clearRunning = true;
+		var millisecondsPerDay = 1000 * 60 * 60 * 24;
+		var today = (new Date()).getTime() - millisecondsPerDay;
 
-chrome.tabs.onUpdated.addListener(function(tabId, props) {
-  if (props.status == "complete")
-    c();
+		chrome.browsingData.removeCache({
+			"since" : aboutAWeekAgo
+		}, function () {
+			clearRunning = false;
+		});
+	} else {
+		alert("Your browser does not support 'chrome.browsingData.removeCache'.");
+	}
+
+};
+
+chrome.tabs.onUpdated.addListener(function (tabId, props) {
+	if (props.status == "complete")
+		clearCache();
 });
