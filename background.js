dbd4e97aa8c98e8e59fb616dfbdc5a66a9666aaa
adotland <a@dotlandconsulting.com>
2015-05-16 23:32:36 -0700

    open seattlefoodtruck.com/by-day/[today] on install, bug fixes

--- background.js
+++ background.js
@@ -1,21 +1,39 @@
+var weekdays = new Array(7);
+weekdays[0] = "Sunday";
+weekdays[1] = "Monday";
+weekdays[2] = "Tuesday";
+weekdays[3] = "Wednesday";
+weekdays[4] = "Thursday";
+weekdays[5] = "Friday";
+weekdays[6] = "Saturday";
+
+var now = new Date();
+
+var dayOfWeek = weekdays[now.getDay()];
+
 var clearCache = function () {
 	if (typeof(chrome.browsingData) !== 'undefined') {
 		clearRunning = true;
 		var millisecondsPerDay = 1000 * 60 * 60 * 24;
-		var today = (new Date()).getTime() - millisecondsPerDay;
+		var today = now.getTime() - millisecondsPerDay;
 
 		chrome.browsingData.removeCache({
-			"since" : aboutAWeekAgo
+			"since" : today
 		}, function () {
 			clearRunning = false;
 		});
 	} else {
 		alert("Your browser does not support 'chrome.browsingData.removeCache'.");
 	}
-
 };
 
 chrome.tabs.onUpdated.addListener(function (tabId, props) {
-	if (props.status == "complete")
+	if (props.status === "complete")
 		clearCache();
+});
+
+chrome.runtime.onInstalled.addListener(function (details){
+    if(details.reason === "install"){
+        chrome.tabs.create({url: "http://www.seattlefoodtruck.com/index.php/by-day/" + dayOfWeek + "/"});
+    }
 });
